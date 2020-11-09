a48f1a9dbfd2e15b7b9778f614364659498ca410
Pietro Anello <pietroanello.dev@gmail.com>
2020-11-09 19:03:38 +0100

    added usercontext info

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -3,13 +3,30 @@ import React, { useState, useEffect } from "react"
 const UserContext = React.createContext()
 
 function UserContextProvider(props) {
+    const [isLogged, setIsLogged] = useState(false)
+    const [isSigned, setIsSigned] = useState(false)
     const [userInfo, setUserInfo] = useState({
         firstName: null,
         lastName: null,
     })
 
+    useEffect(() => {
+        fetch("/api/auth/prova")
+            .then(res => res.json())
+            .then(data => console.log(data))
+    }, [])
+
     return (
-        <UserContext.Provider value={{ userInfo, setUserInfo }}>
+        <UserContext.Provider
+            value={{
+                userInfo,
+                setUserInfo,
+                isLogged,
+                setIsLogged,
+                isSigned,
+                setIsSigned,
+            }}
+        >
             {props.children}
         </UserContext.Provider>
     )
