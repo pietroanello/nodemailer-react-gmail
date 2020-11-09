1bf06200120dbf9debd3b53796be5debf01e212e
Pietro Anello <pietroanello.dev@gmail.com>
2020-11-09 18:08:20 +0100

    added usercontext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-import React from "react"
+import React, { useState, useContext } from "react"
+import SignIn from "./components/SignIn"
+import SignUp from "./components/SignUp"
+import UserContext from "./context/UserContext"
 
 function App() {
-    return <></>
+    const [isLogged, setIsLogged] = useState(false)
+    const [isSigned, setIsSigned] = useState(false)
+    const { firstName, lastName } = useContext(UserContext)
+
+    return (
+        <>
+            <header>
+                <h1>Welcome!</h1>
+                {isLogged ? (
+                    <p>Welcome {firstName + lastName}</p>
+                ) : (
+                    <p>
+                        Sign Up for access or Sing In if you already have an
+                        account!
+                    </p>
+                )}
+            </header>
+            {isLogged ? "" : isSigned ? <SignIn /> : <SignUp />}
+        </>
+    )
 }
 
 export default App
---
+++ src/context/UserContext.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from "react"
+
+const UserContext = React.createContext
+
+function UserContextProvider(props) {
+    const [userInfo, setUserInfo] = useState({
+        firstName: null,
+        lastName: null,
+    })
+
+    return (
+        <UserContext.Provider value={(userInfo, setUserInfo)}>
+            {props.children}
+        </UserContext.Provider>
+    )
+}
+
+export { UserContextProvider, UserContext }
