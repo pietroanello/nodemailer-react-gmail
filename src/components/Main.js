5e3d37246e8efe7b9a12db5a532438b7131753da
Pietro Anello <pietroanello.dev@gmail.com>
2020-11-10 16:46:06 +0100

    added simple lottie animation on login

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,24 @@ import { Button } from "@material-ui/core"
 import React from "react"
 import ExitToAppIcon from "@material-ui/icons/ExitToApp"
 import useRegisterLogin from "../hooks/useRegisterLogin"
+import * as animationData from "../animations/anim.json"
+import Lottie from "react-lottie"
 
 export default function Main() {
     const { signOut } = useRegisterLogin()
+
+    const defaultOptions = {
+        renderer: "svg",
+        loop: false,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+            preserveAspectRatio: "xMidYMid slice",
+        },
+    }
+
     return (
-        <>
+        <main>
             <Button
                 onClick={signOut}
                 variant='contained'
@@ -15,6 +28,19 @@ export default function Main() {
             >
                 Logout
             </Button>
-        </>
+            <Lottie
+                style={{
+                    position: "absolute",
+                    bottom: "0px",
+                    left: "0px",
+                    zIndex: "-1",
+                }}
+                options={defaultOptions}
+                width={"100%"}
+                isStopped={false}
+                isPaused={false}
+                isClickToPauseDisabled={true}
+            />
+        </main>
     )
 }
